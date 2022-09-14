c627da54335bc43ccbdf3d876c5eee5252709f69
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-09-14 18:11:36 +0200

    Icon Component Done
    May need to tweak some CSS!

--- client/src/Routes/Home/Home.jsx
+++ client/src/Routes/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import styles from './Home.module.scss';
 
 const Home = () => {
     return (
-        <div>
+        <div className={styles.container}>
             
         </div>
     );
