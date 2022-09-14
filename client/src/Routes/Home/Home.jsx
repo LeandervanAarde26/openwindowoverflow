2b33144e5a6ef4418820f8a29f2307f9100176c8
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-14 19:14:07 +0200

    Added style

--- client/src/Routes/Home/Home.jsx
+++ client/src/Routes/Home/Home.jsx
@@ -1,9 +1,10 @@
 import styles from './Home.module.scss';
+import AllButtons from '../../Components/ButtonExamples/AllButtons.component';
 
 const Home = () => {
     return (
         <div className={styles.container}>
-            
+            <AllButtons/>
         </div>
     );
 };
