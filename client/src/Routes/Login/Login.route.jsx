be00edf46893be4e77f23dd3c520fe66e07edae5
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-19 11:28:48 +0200

    Password reset moved

--- client/src/Routes/Login/Login.route.jsx
+++ client/src/Routes/Login/Login.route.jsx
@@ -26,14 +26,14 @@ const Login = () => {
 
     const handleChange = (e) => {
         // This refers to e.target.name && e.target.value
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         // Checks for email ending with virtualwindow.co.za || a email ending with openwindow.co.za
         const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         let correctEmail = value.includes("@openwindow.co.za") || value.includes("@virtualwindow.co.za");
         // Check if the correct email is supplied by checking it against the regex
         const emailCheck = emailRegex.test(value);
 
-        setFormValues({...formValues, [name]: value});
+        setFormValues({ ...formValues, [name]: value });
 
         if (value.length > 2) {
             if (emailCheck && correctEmail) {
@@ -51,56 +51,57 @@ const Login = () => {
 
     return (
         <div className={styles.outer}>
-                {/* left div, where image is going to be  */}
-                <div className={styles.left}></div>
-                {/* Right div aka login form */}
-                <div className={styles.right}>
-                    <h2 className={styles.heading}>Login</h2>
-                    <form>
-                        <Input
-                            id={error ? styles.err : ""}
-                            label={error ? "Invalid email, please try again" : "Email"}
-                            value={email}
-                            type="email"
-                            name="email"
-                            required={true}
-                            onChange={handleChange}
-                        />
+            {/* left div, where image is going to be  */}
+            <div className={styles.left}></div>
+            {/* Right div aka login form */}
+            <div className={styles.right}>
+                <h2 className={styles.heading}>Login</h2>
+                <form>
+                    <Input
+                        id={error ? styles.err : ""}
+                        label={error ? "Invalid email, please try again" : "Email"}
+                        value={email}
+                        type="email"
+                        name="email"
+                        required={true}
+                        onChange={handleChange}
+                    />
 
-                        <Input
-                            label={"Password"}
-                            value={password}
-                            type="password"
-                            name="password"
-                            required={true}
-                            onChange={handleChange}
-                        />
-                    </form>
+                    <Input
+                        label={"Password"}
+                        value={password}
+                        type="password"
+                        name="password"
+                        required={true}
+                        onChange={handleChange}
+                    />
+                </form>
 
-                    {
-                        clickable ?
-                            <>
-                                <Button
-                                    buttonType={'black'}
-                                    children={"Sign In"}
-                                    buttonSize={styles.buttonSize}
-                                />
+                {
+                    clickable ?
+                        <>
+                            <p className={styles.reset}>Forgot password? <br /> <span>Click here to reset</span></p>
+                            <Button
+                                buttonType={'black'}
+                                children={"Sign In"}
+                                buttonSize={styles.buttonSize}
+                            />
 
-                                <p className={styles.option}>Don't have an Account?</p>
+                            <p className={styles.option}>Don't have an Account?</p>
 
-                                <Button
-                                    buttonType={'secondary'}
-                                    children={'Sign Up'}
-                                    buttonSize={styles.secondary}
-                                    onClick={Register}
-                                />
-                            </>
-                            :
-                            ""
-                    }
-                </div>
+                            <Button
+                                buttonType={'secondary'}
+                                children={'Sign Up'}
+                                buttonSize={styles.secondary}
+                                onClick={Register}
+                            />
+                        </>
+                        :
+                        ""
+                }
+            </div>
 
-                <Outlet />
+            <Outlet />
         </div>
     );
 };
