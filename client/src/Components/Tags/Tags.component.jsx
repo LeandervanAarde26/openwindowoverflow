c8588aa264cffb0a86fd158ea93efc2ebaa900c9
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-26 21:01:17 +0200

    Followed tags fixe
    Followed tags fixed

--- client/src/Components/Tags/Tags.component.jsx
+++ client/src/Components/Tags/Tags.component.jsx
@@ -4,9 +4,7 @@ import styles from "./Tags.module.scss"
 const Tags = ({title}) => {
     return (
         <div className={styles.container}>
-           <p>
             {title}
-           </p>
         </div>
     );
 };
