ebd0ce987b00afd8ba131db7a546ba954c10d476
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-26 20:52:35 +0200

    Answers component started

---
+++ client/src/Components/Answers/Answers.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Answers.module.scss";
+
+const Answers = () => {
+    return (
+        <div className={styles.outer}>
+            <h5>4 Answers</h5>
+        </div>
+    );
+};
+
+export default Answers;
--- client/src/Routes/Components/Components.js
+++ client/src/Routes/Components/Components.js
@@ -38,18 +38,15 @@ const Components = () => {
             
             {/* <MyQuestionsAnswersContainer/> */}
 
-<<<<<<< HEAD
             {/* <CommentsContainer /> */}
 
 
             <RightContainer />
-=======
+
 
 
 
             {/*<RightContainer /> */}
->>>>>>> 436d41bde356b9138a8e559516c86d55f636af37
-
         </div>
     );
 };
