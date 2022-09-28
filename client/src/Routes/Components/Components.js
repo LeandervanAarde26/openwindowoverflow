4e8bac6097811b83dc10b5b1f6e910e2278c0c3f
LeandervanAarde <200211@virtualwindow.co.za>
2022-09-28 15:12:25 +0200

    I cant remember what this was

--- client/src/Routes/Components/Components.js
+++ client/src/Routes/Components/Components.js
@@ -14,6 +14,7 @@ import QuestionsContainer from '../../Components/QuestionsContainer/QuestionsCon
 import MyQuestionsAnswersContainer from '../../Components/MyQuestionsAnswersContainer/MyQuestionsAnswersContainer.component';
 import PostedFollowedArticlesContainer from '../../Components/PostedFollowedArticlesContainer/PostedFollowedArticlesContainer.component';
 import ArticlesFilterContainer from '../../Components/ArticlesFilterContainer/ArticlesFilterContainer.component';
+import Answers from '../../Components/Answers/Answers.component';
 
 
 
@@ -32,7 +33,8 @@ const Components = () => {
             <SideNavigation />
             {/* <CommentsContainer /> */}
 
-            <ArticlesFilterContainer/>
+            {/* <ArticlesFilterContainer/> */}
+            <Answers/>
 
             {/* <PostedFollowedArticlesContainer/> */}
             
