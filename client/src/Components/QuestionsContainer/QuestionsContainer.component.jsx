b0a507801ebc8720401471234b38fb466060aaab
LeandervanAarde <200211@virtualwindow.co.za>
2022-10-03 00:10:39 +0200

    badges removed from QuestionsContainer
    Badges been removed from QuestionsContainer..

--- client/src/Components/QuestionsContainer/QuestionsContainer.component.jsx
+++ client/src/Components/QuestionsContainer/QuestionsContainer.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Badges from '../Badges/Badges.component';
 import Preview from '../Preview/Preview.component';
 import styles from "./QuestionsContainer.module.scss";
 
@@ -18,28 +17,7 @@ const QuestionsContainer = () => {
                     <Preview />
                     <Preview />
                     <Preview />
-                    <Badges badgeType={"level1"}/>
-                    <Badges badgeType={"level2"}/>
-                    <Badges badgeType={"level3"}/>
-                    <Badges badgeType={"level4"}/>
-                    <Badges badgeType={"level5"}/>
-                    <Badges badgeType={"level6"}/>
-                    <Badges badgeType={"level7"}/>
-                    <Badges badgeType={"level8"}/>
-                    <Badges badgeType={"level9"}/>
-                    <Badges badgeType={"bug"}/>
-                    <Badges badgeType={"fox"}/>
-                    <Badges badgeType={"happypc"}/>
-                    <Badges badgeType={"idea"}/>
-                    <Badges badgeType={"multiscreen"}/>
-                    <Badges badgeType={"network"}/>
-                    <Badges badgeType={"phoneapp"}/>
-                    <Badges badgeType={"studybot"}/>
-                    <Badges badgeType={"syntax"}/>
-                    <Badges badgeType={"wireframe"}/>
-                    <Badges badgeType={"teamwork"}/>
-
-
+                
                 </div>
             </div>
         </>
