7416096cb64a35c884aac8544c254755eca31b09
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-10-03 20:43:36 +0200

    PostAnswer
    Just need to import the icons

--- client/src/Components/PostAnswer/PostAnswer.component.jsx
+++ client/src/Components/PostAnswer/PostAnswer.component.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import styles from './PostAnswer.module.scss';
+import Button from '../Button/Button.component'
 
 const PostAnswer = () => {
 
     return (
         <div className={styles.container}>
-            <h5>Your Answer</h5>
+            <h5>Your answer</h5>
             <div className={styles.icons}>
-                <h5>Images go here</h5>
+                
             </div>
 
                 <div className={styles.information}>
                 </div>
-            <button/>
+
+            <div className={styles.button}>
+                <Button
+                    buttonType={'primary'}
+                    children={"Post answer"}
+                />
+            </div>
         </div>
     );
 };
 
 export default PostAnswer;
+
+// /* Icons/Images */
+// import logo from '../../Assets/Icons/testLogo.svg';
+// import trophy from "../../Assets/Icons/ic_trophy.svg";
+// import inbox from "../../Assets/Icons/ic_inbox.svg";
+// import Icon from '../Icon/Icon';
+
+
+{/* <Icon
+className={styles['containerOuter__login--trophy']}
+icon={inbox}
+/> */}
