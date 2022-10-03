7e1dbc2ca26c73b0e9289fa56c583f4d69fe3545
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-10-03 11:53:38 +0200

    Refactored Code

--- client/src/Routes/Components/Components.js
+++ client/src/Routes/Components/Components.js
@@ -3,12 +3,13 @@ import AdSenseContainer from "../../Components/AdSenseContainer/AdSenseContainer
 import Answers from "../../Components/Answers/Answers.component";
 import ArticlesContainer from "../../Components/ArticlesContainer/ArticlesContainer.component";
 import ArticlesFilter from "../../Components/ArticlesFilter/ArticlesFilter.component";
+import ArticlesFilterContainer from "../../Components/ArticlesFilterContainer/ArticlesFilterContainer.component";
 import styles from "./Components.module.scss";
 
 const Components = () => {
   return (
     <div className={styles.container}>
-        <ArticlesFilter/>
+        <ArticlesFilterContainer/>
     </div>
   );
 };
