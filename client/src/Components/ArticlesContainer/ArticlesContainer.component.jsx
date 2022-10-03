a743ac215621889f45ab487cfa89b7293e177da0
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-10-03 10:09:54 +0200

    ArticlesContainer Component Updated
    Removed Media Queries

--- client/src/Components/ArticlesContainer/ArticlesContainer.component.jsx
+++ client/src/Components/ArticlesContainer/ArticlesContainer.component.jsx
@@ -4,21 +4,21 @@ import styles from "./ArticlesContainer.module.scss";
 const ArticlesContainer = () => {
     return (
         <>
-        <div className={styles.outer}>
-            <h3>Articles</h3>
-            <div className={styles.container}>
-                <Preview 
-                    goodQuestion={true}
-                    resolved={true}
-                />
-                <Preview />
-                <Preview />
-                <Preview />
-                <Preview />
-                <Preview />
+            <div className={styles.outer}>
+                <h3>Articles</h3>
+                <div className={styles.container}>
+                    <Preview 
+                        goodQuestion={true}
+                        resolved={true}
+                    />
+                    <Preview />
+                    <Preview />
+                    <Preview />
+                    <Preview />
+                    <Preview />
+                </div>
             </div>
-        </div>
-    </>
+        </>
     );
 };
 
--- client/src/Routes/Components/Components.js
+++ client/src/Routes/Components/Components.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AdSenseContainer from "../../Components/AdSenseContainer/AdSenseContainer.component";
 import Answers from "../../Components/Answers/Answers.component";
+import ArticlesContainer from "../../Components/ArticlesContainer/ArticlesContainer.component";
 import styles from "./Components.module.scss";
 
 const Components = () => {
   return (
     <div className={styles.container}>
-      <Answers />
+        <ArticlesContainer/>
     </div>
   );
 };
