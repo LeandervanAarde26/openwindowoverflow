389049f20dc5530f751e51c8240dd4280ebde6bc
Liam Wedge <21100218@virtualwindow.co.za>
2022-10-12 09:15:48 +0200

    svg and spelling

--- client/src/Components/NavigationBar/NavBar.jsx
+++ client/src/Components/NavigationBar/NavBar.jsx
@@ -1,90 +1,71 @@
 /* React */
-import {NavLink} from 'react-router-dom';
-import { useNavigate } from 'react-router';
-import { useContext } from 'react';
-import { RegisterContext } from '../../Contexts/Register.context';
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { RegisterContext } from "../../Contexts/Register.context";
 
 /* Styling */
-import styles from './NavBar.module.scss';
+import styles from "./NavBar.module.scss";
 
 /* Components */
-import SearchBar from '../Input/SearchBar';
-import Icon from '../Icon/Icon';
-import Button from '../Button/Button.component'
-import ProfileCard from '../Profile/ProfileCard';
-import Dropdown from '../Dropdown/Dropdown';
+import SearchBar from "../Input/SearchBar";
+import Icon from "../Icon/Icon";
+import Button from "../Button/Button.component";
+import ProfileCard from "../Profile/ProfileCard";
+import Dropdown from "../Dropdown/Dropdown";
 
 /* Icons/Images */
-import logo from '../../Assets/Icons/testLogo.svg';
+import logo from "../../Assets/Icons/testLogo.svg";
 import trophy from "../../Assets/Icons/ic_trophy.svg";
 import inbox from "../../Assets/Icons/ic_inbox.svg";
 
 const NavBar = () => {
+  const user = sessionStorage.getItem("currentUser");
 
-    const user = sessionStorage.getItem("currentUser")
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-
-    const goToProfile = () => {
-        // navigate("/Profile")
-        navigate(`/profile/${user}`)
-    }
-    return (
-        <div className={styles.container}>
-            <div className={styles.containerOuter}>
-                <div className={styles.containerOuter__logo}>
-                    <img
-                        src={logo} 
-                        alt="" 
-                    />
-                </div>
-                <div className={styles.containerOuter__search}>
-                    <SearchBar
-                        label={"Search"}
-                        placeholder={"Search..."}
-                    />
-                </div>
-                <div className={styles.containerOuter__login}>
-                    <Button
-                        buttonType={'black'}
-                        children={"Sign out"}
-                    />
-
-                    <div className={styles['containerOuter__login--dropdown']}>
-                        <NavLink to='/'>
-                            <Icon
-                                className={styles['containerOuter__login--trophy']}
-                                icon={trophy}
-                            />
-                        </NavLink>
-                        <div className={styles.dropContent}>
-                            <Dropdown
-                                title={'Notifications'}
-                            />
-                        </div>
-                    </div>
-
-                    <Icon
-                        className={styles['containerOuter__login--trophy']}
-                        icon={inbox}
-                    />
-                </div>
-                <div className={styles.containerOuter__profile}>
-                    <ProfileCard
-                        profileImage={logo}
-                        function ={goToProfile}
-                    />
-                </div>
-            </div>
-            <div className={styles.container__search}>
-                <SearchBar
-                    label={"Search"}
-                    placeholder={"Search..."}
-                />
-            </div>
+  const goToProfile = () => {
+    // navigate("/Profile")
+    navigate(`/profile/${user}`);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerOuter}>
+        <div className={styles.containerOuter__logo}>
+          <img src={logo} alt="" />
         </div>
-    );
+        <div className={styles.containerOuter__search}>
+          <SearchBar label={"Search"} placeholder={"Search..."} />
+        </div>
+        <div className={styles.containerOuter__login}>
+          <Button buttonType={"black"} children={"Sign out"} />
+
+          <div className={styles["containerOuter__login--dropdown"]}>
+            <NavLink to="/">
+              <Icon
+                className={styles["containerOuter__login--trophy"]}
+                icon={trophy}
+              />
+            </NavLink>
+            <div className={styles.dropContent}>
+              <Dropdown title={"Notifications"} />
+            </div>
+          </div>
+
+          <Icon
+            className={styles["containerOuter__login--trophy"]}
+            icon={inbox}
+          />
+        </div>
+        <div className={styles.containerOuter__profile}>
+          <ProfileCard profileImage={logo} function={goToProfile} />
+        </div>
+      </div>
+      <div className={styles.container__search}>
+        <SearchBar label={"Search"} placeholder={"Search..."} />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- client/src/Routes/ValidateUser/ValidateUser.component.jsx
+++ client/src/Routes/ValidateUser/ValidateUser.component.jsx
@@ -1,94 +1,103 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import styles from "./ValidateUser.module.scss";
 import theImage from "../../Assets/LoginImage.png";
-import brokenBrowser from "../../Assets/brokenbrowser.png"
+import brokenBrowser from "../../Assets/brokenbrowser.png";
 // import { RegisterContext } from '../../Contexts/Register.context';
-import Button from '../../Components/Button/Button.component';
-import { useNavigate } from 'react-router';
-import { useEffect } from 'react';
-import axios from 'axios'
-import { useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useParams } from 'react-router';
+import Button from "../../Components/Button/Button.component";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { useParams } from "react-router";
 
 const ValidateUser = () => {
-    // const { currentUser } = useContext(RegisterContext)
-    const [searchParams] = useSearchParams();
-    const user = useParams()
-    // const curr = currentUser.currentUser
-    const navigate = useNavigate();
-    const [content, setContent] = useState()
-    const [login, setLogin] = useState(false)
-    console.log(searchParams.get('id'))
-  
-    useEffect(() => {
-        axios.patch(`http://localhost:5001/api/validateUser/${searchParams.get('id')}`)
-            .then(res => {
-                console.log(res)
-                if (res.status === 200) {
-                    setContent(
-                        <h3>Welcome new user!
-                            <br />
-                            Your account has succesfully verified! Let's login and get started!
-                        </h3>
-                    )
-                    setLogin(true)
-                } else {
-                    setContent(
-                        <>
-                            <h3>OOPS!</h3>
-                            <h5>
-                                We could not verify your new account, please try again or contact support at
-                                mail@openoverflow.co.za
-                            </h5>
-                        </>
-                    )
-                    setLogin(false)
-                }
-            })
-            .catch(err => {
-                console.log(err)
+  // const { currentUser } = useContext(RegisterContext)
+  const [searchParams] = useSearchParams();
+  const user = useParams();
+  // const curr = currentUser.currentUser
+  const navigate = useNavigate();
+  const [content, setContent] = useState();
+  const [login, setLogin] = useState(false);
+  console.log(searchParams.get("id"));
 
-                setContent(
-                    <>
-                        <h3>OOPS!</h3>
-                        <h5>
-                            We could not verify your new account, please try again or contact support at
-                            mail@openoverflow.co.za
-                        </h5>
-                    </>
-                )
-                setLogin(false)
-            })
-    }, [])
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <img className='img' src={login ? theImage : brokenBrowser} alt={"ValidationImage"} />
-            </div>
-            <div className={styles.right}>
-                {content}
-                <div className={styles.buttonContainer}>
-                    {/* Link didnt work because it is looking for a local path to redirect ti, a href takes anylink */}
-                    {
-                        login
-                            ?
-                            <Button
-                                buttonType={"primary"}
-                                children={"Go to Login"}
-                                onClick={() => { navigate("/") }}
-                            />
-                            :
-                            <Button
-                                buttonType={"black"}
-                                children={"Register Again"}
-                                onClick={() => { navigate("/Register") }}
-                            />
-                    }
-                </div>
-            </div>
+  useEffect(() => {
+    axios
+      .patch(`http://localhost:5001/api/validateUser/${searchParams.get("id")}`)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setContent(
+            <h3>
+              Welcome new user!
+              <br />
+              Your account has successfully verified! Let's login and get
+              started!
+            </h3>
+          );
+          setLogin(true);
+        } else {
+          setContent(
+            <>
+              <h3>OOPS!</h3>
+              <h5>
+                We could not verify your new account, please try again or
+                contact support at mail@openoverflow.co.za
+              </h5>
+            </>
+          );
+          setLogin(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+
+        setContent(
+          <>
+            <h3>OOPS!</h3>
+            <h5>
+              We could not verify your new account, please try again or contact
+              support at mail@openoverflow.co.za
+            </h5>
+          </>
+        );
+        setLogin(false);
+      });
+  }, []);
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img
+          className="img"
+          src={login ? theImage : brokenBrowser}
+          alt={"ValidationImage"}
+        />
+      </div>
+      <div className={styles.right}>
+        {content}
+        <div className={styles.buttonContainer}>
+          {/* Link didnt work because it is looking for a local path to redirect ti, a href takes anylink */}
+          {login ? (
+            <Button
+              buttonType={"primary"}
+              children={"Go to Login"}
+              onClick={() => {
+                navigate("/");
+              }}
+            />
+          ) : (
+            <Button
+              buttonType={"black"}
+              children={"Register Again"}
+              onClick={() => {
+                navigate("/Register");
+              }}
+            />
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ValidateUser;
