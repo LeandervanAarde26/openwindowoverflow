7241a7b5c93b467a6fff930e47f876464bad8f37
Liam Wedge <21100218@virtualwindow.co.za>
2022-10-18 23:51:25 +0200

    nav bar and questions stashes

--- client/src/Routes/ValidateUser/ValidateUser.component.jsx
+++ client/src/Routes/ValidateUser/ValidateUser.component.jsx
@@ -1,100 +1,106 @@
 /* React */
-import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import axios from 'axios'
-import { useSearchParams } from 'react-router-dom';
-import { useParams } from 'react-router';
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router";
 
 /* Styling */
 import styles from "./ValidateUser.module.scss";
 
-/* Components */
-import Button from '../../Components/Button/Button.component';
-
 /* Icons/Images */
 import theImage from "../../Assets/LoginImage.png";
-import brokenBrowser from "../../Assets/brokenbrowser.png"
+import brokenBrowser from "../../Assets/brokenbrowser.png";
 // import { RegisterContext } from '../../Contexts/Register.context';
 
 const ValidateUser = () => {
-    // const { currentUser } = useContext(RegisterContext)
-    const [searchParams] = useSearchParams();
-    const user = useParams()
-    // const curr = currentUser.currentUser
-    const navigate = useNavigate();
-    const [content, setContent] = useState()
-    const [login, setLogin] = useState(false)
-    console.log(searchParams.get('id'))
-  
-    useEffect(() => {
-        axios.patch(`http://localhost:5001/api/validateUser/${searchParams.get('id')}`)
-            .then(res => {
-                console.log(res)
-                if (res.status === 200) {
-                    setContent(
-                        <h3>Welcome new user!
-                            <br />
-                            Your account has succesfully verified! Let's login and get started!
-                        </h3>
-                    )
-                    setLogin(true)
-                } else {
-                    setContent(
-                        <>
-                            <h3>OOPS!</h3>
-                            <h5>
-                                We could not verify your new account, please try again or contact support at
-                                mail@openoverflow.co.za
-                            </h5>
-                        </>
-                    )
-                    setLogin(false)
-                }
-            })
-            .catch(err => {
-                console.log(err)
+  // const { currentUser } = useContext(RegisterContext)
+  const [searchParams] = useSearchParams();
+  const user = useParams();
+  // const curr = currentUser.currentUser
+  const navigate = useNavigate();
+  const [content, setContent] = useState();
+  const [login, setLogin] = useState(false);
+  console.log(searchParams.get("id"));
 
-                setContent(
-                    <>
-                        <h3>OOPS!</h3>
-                        <h5>
-                            We could not verify your new account, please try again or contact support at
-                            mail@openoverflow.co.za
-                        </h5>
-                    </>
-                )
-                setLogin(false)
-            })
-    }, [])
+  useEffect(() => {
+    axios
+      .patch(`http://localhost:5001/api/validateUser/${searchParams.get("id")}`)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setContent(
+            <h3>
+              Welcome new user!
+              <br />
+              Your account has succesfully verified! Let's login and get
+              started!
+            </h3>
+          );
+          setLogin(true);
+        } else {
+          setContent(
+            <>
+              <h3>OOPS!</h3>
+              <h5>
+                We could not verify your new account, please try again or
+                contact support at mail@openoverflow.co.za
+              </h5>
+            </>
+          );
+          setLogin(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <img className='img' src={login ? theImage : brokenBrowser} alt={"ValidationImage"} />
-            </div>
-            <div className={styles.right}>
-                {content}
-                <div className={styles.buttonContainer}>
-                    {/* Link didnt work because it is looking for a local path to redirect ti, a href takes anylink */}
-                    {
-                        login
-                        ?
-                            <Button
-                                buttonType={"primary"}
-                                children={"Go to Login"}
-                                onClick={() => { navigate("/") }}
-                            />
-                        :
-                            <Button
-                                buttonType={"black"}
-                                children={"Register Again"}
-                                onClick={() => { navigate("/Register") }}
-                            />
-                    }
-                </div>
-            </div>
+        setContent(
+          <>
+            <h3>OOPS!</h3>
+            <h5>
+              We could not verify your new account, please try again or contact
+              support at mail@openoverflow.co.za
+            </h5>
+          </>
+        );
+        setLogin(false);
+      });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img
+          className="img"
+          src={login ? theImage : brokenBrowser}
+          alt={"ValidationImage"}
+        />
+      </div>
+      <div className={styles.right}>
+        {content}
+        <div className={styles.buttonContainer}>
+          {/* Link didnt work because it is looking for a local path to redirect ti, a href takes anylink */}
+          {login ? (
+            <Button
+              buttonType={"primary"}
+              children={"Go to Login"}
+              onClick={() => {
+                navigate("/");
+              }}
+            />
+          ) : (
+            <Button
+              buttonType={"black"}
+              children={"Register Again"}
+              onClick={() => {
+                navigate("/Register");
+              }}
+            />
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ValidateUser;
