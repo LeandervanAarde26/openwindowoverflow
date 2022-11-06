925a7c651c4ef338013d1b8cc95c2031f9b58f26
Godwin Cameron <200109@virtualwindow.co.za>
2022-11-06 12:53:57 +0200

    mapped flagged comments

--- client/src/Components/FlaggedComment/FlaggedComment.component.jsx
+++ client/src/Components/FlaggedComment/FlaggedComment.component.jsx
@@ -1,5 +1,6 @@
 /* React */
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 /* Styling */
 import styles from './FlaggedComment.module.scss';
@@ -11,12 +12,20 @@ import Button from '../Button/Button.component';
 /* Icons/Images */
 import ic_user from '../../Assets/Icons/ic_user.svg';
 import ic_flag from '../../Assets/Icons/ic_flag.svg';
+import ic_bin from '../../Assets/Icons/ic_bin.svg';
 
-const FlaggedComment = () => {
+const FlaggedComment = (props) => {
+    const navigate = useNavigate()
+
+    const { commentTitle, questionId, askedUser, commentUser } = props
+
+    const goToQuestion = () => { navigate(`/Question/${questionId}`) }
+
     return (
         <div className={styles.container}>
             <div className={styles.container__top}>
-                <h4>User flagged users comment on question This is thsfas fasfsfa sfasfe title of the question that the user h</h4>
+                <h4>{commentTitle}</h4>
+
             </div>
             <div className={styles.container__info}>
                 <div className={styles['container__info--content']}>
@@ -24,23 +33,30 @@ const FlaggedComment = () => {
                         icon={ic_user}
                     />
 
-                    <p><strong>User</strong> asked question 5 hours ago</p>
+                    <p><strong>{askedUser}</strong> asked question</p>
                 </div>
                 <div className={styles['container__info--content']}>
                     <Icon
                         icon={ic_user}
                     />
 
-                    <p><strong>User</strong> asked question 5 hours ago</p>
+
+                    <p><strong>{commentUser}</strong> commented</p>
                 </div>
             </div>
             <div className={styles.container__bottom}>
                 <Icon
                     icon={ic_flag}
                 />
+                <Icon
+                    className={styles.bin}
+                    icon={ic_bin}
+                />
                 <Button
+                    id={questionId}
                     buttonType='primary'
                     children='View'
+                    onClick={goToQuestion}
                 />
             </div>
         </div>
--- client/src/Routes/Admin/Admin.route.jsx
+++ client/src/Routes/Admin/Admin.route.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Admin.module.scss";
 import SideNavigation from "../../Components/sideNavigation/SideNavigation.component";
 import RightContainer from "../../Components/RightContainer/RightContainer.component";
@@ -9,48 +9,59 @@ import axios from 'axios'
 
 const AdminRoute = () => {
   const [flagged, setFlagged] = useState()
+  const [busy, setBusy] = useState(true)
 
-  useEffect(() =>{
+  useEffect(() => {
 
     axios.get('http://localhost:5001/api/getflagged')
-    .then(res =>{
-      console.log(res.data)
-    })
-    .catch(err =>{
-      console.log(err)
-    })
+      .then(res => {
+        console.log(res.data)
+        setFlagged(res.data)
+        setBusy(false)
+      })
+      .catch(err => {
+        console.log(err)
+      })
 
   }, [])
 
   return (
-    <div className={styles.container}>
-    <SideNavigation />
-    <div className={styles.middle}>
-        <h3>Flagged Comments</h3>
+    busy
+      ?
+      null
+      :
+      <div className={styles.container}>
+        <SideNavigation />
+        <div className={styles.middle}>
+          <h3>Flagged Comments</h3>
           <div className={styles.flaggedContainer}>
             {/* @Cameron please set the state in the useEffect and map the components below to show what is needed 
-              - Question id needs to be present so we can navigate to the Question
-              - Who the asked user us 
-              - Who the commented user is 
-              - Comment needs to be the title. 
-          
-            */}
-             <FlaggedComment/>
-             <FlaggedComment/>
-             <FlaggedComment/>
-             <FlaggedComment/>
-             <FlaggedComment/>
-             <FlaggedComment/>
+             - Question id needs to be present so we can navigate to the Question
+             - Who the asked user us 
+             - Who the commented user is 
+             - Comment needs to be the title. 
+       
+           */}
+            {/* const {commentTitle, questionId, askedUser, commentUser} = props */}
+            {flagged.map(i => (
+              <FlaggedComment
+                key={i._id}
+                commentTitle={i.comments.map(c => c.flagged ? c.comment : null)}
+                questionId={i._id}
+                askedUser={i.author.username}
+                commentUser={i.comments.map(b => b.flagged ? b.user.username : null)}
+              />
+            ))}
           </div>
 
           <h3 className={styles.breakHeading}>Oldest unanswered Questions</h3>
           <div className={styles.flaggedContainer}>
-          {/* Place preview components */}
+            {/* Place preview components */}
 
           </div>
-    </div>
-    <RightContainer/>
-</div>
+        </div>
+        <RightContainer />
+      </div>
   );
 };
 
